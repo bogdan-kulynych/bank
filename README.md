c0c02f7c32554fd9b06db99031cb890d120e1c37
Bogdan <bogdan.kulinich@gmail.com>
2013-11-03 02:40:13 +0200

    Progressing

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+Bank API Server
+---------------
 
-https://github.com/openshift/origin-server/tree/master/cartridges/openshift-origin-cartridge-nodejs/README.md
+
+## Running
+
+
+Requires `node.js v0.10.x`.
+
+Running locally on port `4443`:
+
+    npm install
+    node server.json 4443
+
+
+## Protocol
+
+
+Every client is required to have a key / certificate signed with Bank CA certificate.
+
+Protocol is via HTTPS RESTful API.
+
+
+### Greetings
+
+
+Request:
+
+    GET /api
+    Content-Type: *
+
+Response:
+
+    200 OK
+
+
+### Authentication
+
+
+Request:
+
+    POST /api/auth
+    Content-Type: application/json
+
+    {
+        "card": "<card id>",
+        "pin" : "<PIN>"
+    }
+
+Successful response:
+
+    200 OK
+    Content-Type: application/json
+
+    {
+        "token": "<unique API token>"
+    }
+
+Failed response:
+
+    401 Unauthorized
+
+
+### Actions
+
+    GET /api/balance
+    Authentication: <unique API token>
--- server.js
+++ server.js
@@ -1,22 +1,60 @@
+/**
+ * Bank subsystem
+ * Author: Bogdan Kulynych
+ *
+ * Main communication router
+ */
+
+
 var fs      = require('fs'),
     https   = require('https'),
     express = require('express'),
 
-    config  = require('./config.json');
+    ssl     = require('./config/ssl.json');
 
-// Inferring options from config file
-var options = config;
+
+// CONFIGURATION
+
+
+var options = {};
+
+// Port
+options.port = process.argv[2] || 4443;
+
+// SSL Keys and Certificates
 ['key', 'cert', 'ca'].map(function(name) {
-    options[name] = fs.readFileSync(config[name]);
+    if (name in ssl) {
+        ssl[name] = fs.readFileSync(ssl[name]);
+    }
 });
 
-// Setting api server
+
+// API SERVER
+
+
 var api = module.exports = express();
-api.get('/', function(req, res) {
-    console.log('Incoming request');
+api.use(express.json());
+
+
+// Index
+api.get('/api', function(req, res) {
+    res.send(200);
 });
 
-https.createServer(options, api)
+
+// Authentication
+api.get('/api/auth', function(req, res) {
+    res.send(501);
+});
+
+
+// Balance
+api.get('/api/balance', function(req, res) {
+    res.send(501);
+});
+
+
+https.createServer(ssl, api)
      .listen(options.port, function() {
         console.log('Server is up and running on port ' + options.port);
      });
