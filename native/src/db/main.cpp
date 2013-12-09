638f72a4115bdfdc89eb42a68b1318108d00a82b
Bogdan <bogdan.kulinich@gmail.com>
2013-12-09 22:26:04 +0200

    Changed testing fixtures

--- native/src/db/main.cpp
+++ native/src/db/main.cpp
@@ -20,19 +20,19 @@ int main()
     Account a1("1111111111111111",
                "1111",
                "George Washington",
-               1000.0,
+               50000.0,
                200.0);
 
     Account a2("2222222222222222",
                "2222",
                "Bogdan Kulynych",
-               200.0,
+               730.0,
                50.0);
 
     Account a3("3333333333333333",
                "3333",
                "Jon Snow",
-               10000.0);
+               1000000.0);
 
     m.insertAccount(a1);
     m.insertAccount(a2);
@@ -44,7 +44,7 @@ int main()
     std::cout << "Name: " << m.nameRequest("1111111111111111") << std::endl;
     std::cout << "Available: " << m.availableRequest("1111111111111111") << std::endl;
     std::cout << "Hold: " << m.holdRequest("1111111111111111") << std::endl;
-    m.changeAvailable("2222222222222222", 300);
+    m.changeAvailable("2222222222222222", 720);
     m.changeHold("2222222222222222", 0);
     if (m.supplementaryRequest("1111111111111111") == "") {
         std::cout << "Suppl card: is empty" << std::endl;
@@ -52,14 +52,14 @@ int main()
         std::cout << "Suppl card: " << m.supplementaryRequest("1111111111111111") << std::endl;
     }
     m.changeSupplCard("1111111111111111", "2222222222222222");
-    m.changeMaxSum("1111111111111111", 1000);
+    m.changeMaxSum("1111111111111111", 51000.0);
     if (m.supplementaryRequest("1111111111111111") == "") {
         std::cout << "Suppl card: is empty" << std::endl;
     } else {
         std::cout << "Suppl card: " << m.supplementaryRequest("1111111111111111") << std::endl;
         std::cout << "Max sum: " << m.maxSumRequest("1111111111111111") << std::endl;
     }
-    m.changeMaxSum("2222222222222222", 10050);
+    m.changeMaxSum("2222222222222222", 10000000.0);
     m.changeSupplCard("2222222222222222", "3333333333333333");
     if (m.supplementaryRequest("1111111111111111") == "") {
         std::cout << "Suppl card: is empty" << std::endl;
